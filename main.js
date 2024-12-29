21ee3f2410f80762aae0a312d933b3a743f43703
DeveloperUchida <kouei28poiou@icloud.com>
2024-12-29 09:47:01 +0900

    DataUpdate

---
+++ main.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+    //metaタグを作成
+    const metaTag = document.createElement("meta");
+    metaTag.name = "deckription";
+    metaTag.content = "この画面は現在調整中です";
+
+    //
+});
